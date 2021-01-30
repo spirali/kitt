e0a08e1a86ab1de0a44fa34028f49f4fbd80d53f
Jakub Beránek <berykubik@gmail.com>
2021-01-30 20:47:31 +0100

    Remove grid test

--- tests/test_image.py
+++ tests/test_image.py
@@ -21,21 +21,6 @@ def test_load_image_resize():
     assert img.shape == (224, 224, 3)
 
 
-def test_grid():
-    images = tuple(
-        load_image(data_path(f"grid/{i}.jpg"), bgr=True) for i in range(1, 7)
-    )
-    images = [
-        cv2.resize(image, (image.shape[0] // 4, image.shape[1] // 4))
-        for image in images
-    ]
-    grid = create_image_grid(images, cols=2, border=True)
-    grid_reference = load_image(data_path("grid/grid.png"), bgr=True)
-
-    assert grid_reference.shape == grid.shape
-    assert not np.any(cv2.subtract(grid, grid_reference))
-
-
 def test_polygons_to_mask():
     mask = polygons_to_binary_mask((16, 16), [[(2, 2), (2, 4), (4, 4), (4, 2)]])
     image = np.zeros((16, 16), dtype=np.float)
