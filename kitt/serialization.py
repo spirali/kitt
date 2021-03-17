abe5da96095b8033585d5f7931bcf508a40d4fb5
Jakub Beránek <berykubik@gmail.com>
2021-03-17 10:24:07 +0100

    Allow parametrizion dataclass_from_dict

--- kitt/serialization.py
+++ kitt/serialization.py
@@ -7,5 +7,5 @@ def dataclass_to_dict(obj):
     return dataclasses.asdict(obj)
 
 
-def dataclass_from_dict(cls, data):
-    return dacite.from_dict(cls, data)
+def dataclass_from_dict(cls, data, **kwargs):
+    return dacite.from_dict(cls, data, **kwargs)
