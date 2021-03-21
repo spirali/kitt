15a5e3e2ccdec3a2a498aa01ee1b3fcff720313b
Jakub Beránek <berykubik@gmail.com>
2021-03-21 15:43:57 +0100

    Rework data loaders

--- kitt/image/segmentation/dataloading.py
+++ kitt/image/segmentation/dataloading.py
@@ -2,58 +2,48 @@ from random import Random
 
 import numpy as np
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.utils import Sequence
 
-from ...dataloading import SequenceWrapper
-from ..image import get_image_size
+from ...dataloading import DataLoader, LoaderWrapper
+from ..image import ImageSize, get_image_size
 from .patching import get_patch, get_patches_per_dimension
 
 
-class SegmentationAugmentingSequence(SequenceWrapper):
-    def __init__(self, sequence: Sequence, augmentation_args, seed=None):
+class SegmentationAugmentationLoader(LoaderWrapper):
+    def __init__(self, loader: DataLoader, augmentation_args, seed=None):
         """
-        Sequence that wraps another sequence and augments both inputs and labels using
+        Loader that wraps another loader and augments both inputs and labels using
         keras.ImageDataGenerator.
 
+        Expects that the input loader returns tuples (input, label).
         Intended for image-to-image segmentation tasks.
-
-        All images within a batch are augmented with the same transform.
 
         :param augmentation_args: constructor arguments for keras.ImageDataGenerator
         """
-        super().__init__(sequence)
+        super().__init__(loader)
         self.seed = seed or 0
         self.augmentator = ImageDataGenerator(**augmentation_args)
 
     def __getitem__(self, index):
         self.seed += 1
-        return self._augment_batch(self.sequence[index])
+        x, y = self.loader[index]
+        return self._augment_image(x), self._augment_image(y)
 
-    def _augment_images(self, images):
-        flow = self.augmentator.flow(
-            images, batch_size=images.shape[0], seed=self.seed, shuffle=False
-        )
-        return next(flow)
-
-    def _augment_batch(self, batch):
-        xs, ys = batch
-        xs_augmented, ys_augmented = self._augment_images(xs), self._augment_images(ys)
-
-        return (xs_augmented, ys_augmented)
+    def _augment_image(self, image):
+        assert image.ndim == 3
+        image = np.expand_dims(image, axis=0)
+        flow = self.augmentator.flow(image, batch_size=1, seed=self.seed, shuffle=False)
+        return next(flow)[0]
 
 
-class PatchingSequence(SequenceWrapper):
+class PatchingSamplerLoader(LoaderWrapper):
     """
-    Sequence that wraps a segmentation sequence and generates randomly cropped patches from it.
+    Loader that wraps a segmentation loader and returns randomly cropped patches from it.
+    Expects that the input loader returns tuples (image, label).
     """
 
-    def __init__(self, sequence: Sequence, size: int, stride: int):
-        super().__init__(sequence)
-        batch = sequence[0]
-        image, mask = batch[0][0], batch[1][0]
-        assert get_image_size(image) == get_image_size(mask)
-
-        self.image_size = get_image_size(image)
+    def __init__(self, loader: DataLoader, size: int, stride: int):
+        super().__init__(loader)
+        self.image_size = get_image_size_from_loader(loader)
         self.size = size
         self.stride = stride
         self.patches_per_image = get_patches_per_dimension(
@@ -61,26 +51,21 @@ class PatchingSequence(SequenceWrapper):
         ) * get_patches_per_dimension(self.image_size[1], self.size, self.stride)
         self.random = Random()
 
+    def get_patch(self, image: np.ndarray, index: int) -> np.ndarray:
+        return get_patch(image, size=self.size, stride=self.stride, patch_index=index)
+
     def __getitem__(self, index):
-        xs, ys = self.sequence[index]
-        batch_x = []
-        batch_y = []
-        for index in range(len(xs)):
-            patch_index = self.random.randint(0, self.patches_per_image - 1)
-            batch_x.append(
-                get_patch(
-                    xs[index],
-                    size=self.size,
-                    stride=self.stride,
-                    patch_index=patch_index,
-                )
-            )
-            batch_y.append(
-                get_patch(
-                    ys[index],
-                    size=self.size,
-                    stride=self.stride,
-                    patch_index=patch_index,
-                )
-            )
-        return np.array(batch_x), np.array(batch_y)
+        x, y = self.loader[index]
+        patch_index = self.random.randint(0, self.patches_per_image - 1)
+        x_patch = self.get_patch(x, patch_index)
+        y_patch = self.get_patch(y, patch_index)
+        return x_patch, y_patch
+
+
+def get_image_size_from_loader(loader: DataLoader) -> ImageSize:
+    item = loader[0]
+    assert len(item) == 2
+    image, mask = item
+    image_size = get_image_size(image)
+    assert image_size == get_image_size(mask)
+    return image_size
