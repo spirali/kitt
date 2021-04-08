44a6faf7fcf1bc6f3db082debeab0ef4dfcc9c4a
Jakub Beránek <berykubik@gmail.com>
2021-04-08 11:58:22 +0200

    Add Mattermost training progress announcement callback

--- kitt/integrations/mattermost.py
+++ kitt/integrations/mattermost.py
@@ -1,21 +1,25 @@
 import contextlib
 import getpass
 import logging
+import math
 import os
 import socket
+import sys
+import time
 
 import requests
+from tensorflow import keras
 
 
 @contextlib.contextmanager
 def announce_computation(priority="normal"):
-    url = _get_mattermost_url()
-    if "NO_ANNOUNCE" in os.environ:
+    url = get_announce_url()
+    if not is_announce_enabled():
         url = None
 
     def send(message):
         if url:
-            _send_mattermost_message(message, url)
+            send_mattermost_message(message, url)
 
     user = getpass.getuser()
     device = socket.gethostname()
@@ -35,12 +39,70 @@ def announce_computation(priority="normal"):
         raise e
 
 
-def _get_mattermost_url():
+def is_announce_enabled():
+    return "NO_ANNOUNCE" not in os.environ
+
+
+def get_announce_url():
     return os.environ.get("MM_ANNOUNCE_URL")
 
 
-def _send_mattermost_message(message, url):
+def get_status_url():
+    return os.environ.get("MM_STATUS_URL")
+
+
+def send_mattermost_message(message, url):
     try:
         requests.post(url, json={"text": message})
     except BaseException as e:
         logging.warning(f"Error while announcing computation: {e}")
+
+
+class AnnounceProgressCallback(keras.callbacks.Callback):
+    def __init__(self, nth_epoch=1):
+        """
+        :param nth_epoch: Announce only every n-th epoch
+        """
+        self.epoch_start_time = 0
+        self.nth_epoch = nth_epoch
+
+    def on_epoch_begin(self, epoch, logs=None):
+        self.epoch_start_time = time.time()
+
+    def on_epoch_end(self, epoch, logs=None):
+        if epoch % self.nth_epoch != 0:
+            return
+
+        if not is_announce_enabled():
+            return
+        url = get_status_url()
+        if not url:
+            return
+
+        duration = time.time() - self.epoch_start_time
+        total_epochs = self.params.get("epochs", 100)
+        epoch = epoch + 1
+        completed_pct = epoch / total_epochs
+        progress_bar = draw_progress_bar(20, completed_pct)
+
+        args = " ".join([sys.executable] + sys.argv)
+        metrics_rows = "\n".join(f"| {k} | {v} |" for (k, v) in (logs or {}).items())
+
+        text = f"""
+{getpass.getuser()}: *{args}*
+Epoch {epoch} has finished in {duration:.3f} s
+{epoch}/{total_epochs} ({(completed_pct * 100):.2f} %) {progress_bar}
+
+| Metric | Value |
+| :---: | :---: |
+{metrics_rows}
+""".strip()
+        send_mattermost_message(text, url)
+
+
+def draw_progress_bar(total_bars: int, percent: float) -> str:
+    completed_char = "█"
+    empty_char = "░"
+
+    completed_bars = math.floor(total_bars * percent)
+    return completed_char * completed_bars + empty_char * (total_bars - completed_bars)
