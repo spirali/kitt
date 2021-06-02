5aa8412ab167c951eaac9e31ee404bd00d383034
Vojtech Cima <cima.vojtech@gmail.com>
2021-06-03 00:01:08 +0200

    ENH: Render bounding boxes based on their type

--- kitt/image/objdetect/render.py
+++ kitt/image/objdetect/render.py
@@ -3,40 +3,38 @@ from typing import List
 import cv2
 import numpy as np
 
-from .annotation import AnnotatedBBox, AnnotatedImage
-
-
-def render_annotated_image(
-    annotated: AnnotatedImage, color=(0, 0, 255), line_width=3
-) -> np.ndarray:
-    """Render an image along with its annotations."""
-    assert annotated.image
-    image = annotated.to_numpy()
-    width, height = annotated.width, annotated.height
-
-    for annotation in annotated.annotations:
-        bbox = annotation.bbox.denormalize(width, height).to_int()
-        cv2.rectangle(image, bbox.top_left, bbox.bottom_right, color, line_width)
-    return image
+from .annotation import AnnotatedBBox, AnnotationType
+from ..image import get_image_size
 
 
 def render_annotations(image: np.ndarray, annotations: List[AnnotatedBBox]):
     """Renders bounding boxes onto an image, optionally with labels and probabilities."""
+    color_map = {
+        AnnotationType.GROUND_TRUTH: (255, 255, 255),
+        AnnotationType.PREDICTION: (255, 255, 0),
+    }
+
     for annotation in annotations:
-        box = annotation.bbox.to_int()
-        cv2.rectangle(image, box.top_left, box.bottom_right, (255, 255, 0), 4)
+        box = annotation.bbox.as_denormalized(*get_image_size(image)).to_int()
+        cv2.rectangle(
+            image, box.top_left, box.bottom_right, color_map[annotation.type], 4
+        )
 
         label = annotation.class_name
-        probability = annotation.confidence
+        confidence = annotation.confidence
 
-        if label or probability:
-            text = f"{label} {probability:.2f}".strip()
+        text_fields = []
+        if label:
+            text_fields.append(f"{label}")
+        if confidence:
+            text_fields.append(f"{confidence:.2f}")
+        if text_fields:
             cv2.putText(
                 image,
-                text,
+                " ".join(text_fields),
                 (box.top_left[0] + 20, box.top_left[1] + 40),
                 cv2.FONT_HERSHEY_SIMPLEX,
                 1,  # font scale
-                (255, 0, 255),
+                color_map[annotation.type],
                 2,
             )  # line type
