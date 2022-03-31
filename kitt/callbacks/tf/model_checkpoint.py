353a973c843c442029eac673cb7c15b97b0d487d
Jakub Beránek <berykubik@gmail.com>
2022-03-31 20:59:13 +0200

    Refactor and improve ModelCheckpoint

--- kitt/callbacks/tf/model_checkpoint.py
+++ kitt/callbacks/tf/model_checkpoint.py
@@ -1,22 +1,43 @@
+import dataclasses
 import heapq
 import logging
 import os
-from typing import List
+from pathlib import Path
+from typing import List, Optional
 
 from tensorflow.keras.callbacks import Callback
 
 from ...utils import get_extension
 
 
+@dataclasses.dataclass
+class SavedModel:
+    path: Path
+    metric: float
+    saved_epoch: int
+
+
+@dataclasses.dataclass
+class ModelEntry:
+    model: SavedModel
+    # This model was saved because of `save_every_n_epochs`, so it should not be removed from disk
+    # when it is removed from the best model queue.
+    pinned: bool
+    metric: float
+
+    def __lt__(self, other: "ModelEntry"):
+        return self.metric < other.metric
+
+
 class ModelCheckpoint(Callback):
     def __init__(
-        self,
-        filepath: str,
-        monitor: str,
-        mode: str = "max",
-        save_every_n_epochs: int = None,
-        save_n_best=1,
-        save_optimizer=False,
+            self,
+            filepath: str,
+            monitor: str,
+            mode: str = "max",
+            save_every_n_epochs: int = None,
+            save_n_best=1,
+            save_optimizer=False,
     ):
         """
         :param filepath: Filepath where to save the model. Can contain "epoch" and "<monitor>"
@@ -40,6 +61,8 @@ class ModelCheckpoint(Callback):
         self.save_n_best = save_n_best or 0
         self.save_every_n_epochs = save_every_n_epochs
         self.epochs_since_save = 0
+        self.total_epochs = 0
+        self.last_metric = None
         self.save_optimizer = save_optimizer
 
         assert self.save_every_n_epochs is None or self.save_every_n_epochs > 0
@@ -55,15 +78,25 @@ class ModelCheckpoint(Callback):
         # Invariants
         # self.best_queue[0] is the worst saved model
         # self.best_queue[-1] is the best saved model
-        self.best_queue = []
+        self.best_queue: List[ModelEntry] = []
 
-    def saved_models(self) -> List[str]:
-        return [item[2] for item in self.best_queue]
+    def saved_models(self) -> List[SavedModel]:
+        """
+        Returns saved models ordered from best to worst.
+        """
+        return [entry.model for entry in self.best_queue[::-1]]
+
+    def best_saved_model(self) -> Optional[SavedModel]:
+        if not self.best_queue:
+            return None
+        return self.best_queue[-1].model
 
     def on_epoch_end(self, epoch, logs=None):
         self.epochs_since_save += 1
+        self.total_epochs += 1
 
         metric_value = logs[self.monitor]
+        self.last_metric = metric_value
         path = self.get_filepath(epoch + 1, logs=logs)
         saved = False
 
@@ -74,35 +107,44 @@ class ModelCheckpoint(Callback):
                 saved = True
 
         if self.save_n_best > 0 and self.is_better(metric_value):
-            self.push_better(epoch, metric_value, path, saved)
+            self.push_model_bounded(epoch, metric_value, path, saved)
             if not saved:
                 self.save_model(path)
 
     def on_train_end(self, logs=None):
         directory = os.path.dirname(self.filepath)
-        self.save_model(os.path.join(directory, "final.hdf5"))
+        final_path = os.path.join(directory, "final.hdf5")
+        self.save_model(final_path)
+        self.push_model(self.last_metric, self.last_metric, final_path, pin=True)
 
     def is_better(self, metric_value: float):
         if len(self.best_queue) < self.save_n_best:
             return True
         value = self.metric_map_fn(metric_value)
-        return value > self.best_queue[0][0]
+        return value > self.best_queue[0].metric
 
-    def push_better(self, epoch: int, metric_value: float, path: str, pin=False):
+    def push_model(self, epoch: int, metric_value: float, path: str, pin: bool):
         value = self.metric_map_fn(metric_value)
-        heapq.heappush(self.best_queue, (value, epoch, path, pin))
+        heapq.heappush(self.best_queue, ModelEntry(
+            model=SavedModel(metric=metric_value, saved_epoch=epoch, path=Path(path)),
+            metric=value,
+            pinned=pin
+        ))
+
+    def push_model_bounded(self, epoch: int, metric_value: float, path: str, pin: bool):
+        self.push_model(epoch=epoch, metric_value=metric_value, path=path, pin=pin)
         if len(self.best_queue) > self.save_n_best:
-            _, _, previous_path, is_pinned = heapq.heappop(self.best_queue)
-            if not is_pinned:
+            entry = heapq.heappop(self.best_queue)
+            if not entry.pinned:
                 try:
-                    os.unlink(previous_path)
+                    os.unlink(entry.model.path)
                 except IOError as e:
                     logging.error(
-                        f"Could not remove previously stored model path {previous_path}: {e}"
+                        f"Could not remove previously stored model path {entry.model.path}: {e}"
                     )
 
     def save_model(self, path: str):
         self.model.save(path, overwrite=True, include_optimizer=self.save_optimizer)
 
-    def get_filepath(self, epoch, logs):
+    def get_filepath(self, epoch, logs) -> str:
         return self.filepath.format(epoch=epoch, **logs)
