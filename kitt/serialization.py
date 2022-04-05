6b525dca00c2139b680013d0b351d41296eb86bd
Jakub Beránek <berykubik@gmail.com>
2022-04-05 16:32:37 +0200

    Move YAML serialization to kitt.serialization from kitt.environment
    Backwards compatibility is preserved by an alias.

--- kitt/serialization.py
+++ kitt/serialization.py
@@ -1,6 +1,7 @@
 import dataclasses
 
 import dacite
+from yaml import SafeDumper
 
 
 def dataclass_to_dict(obj):
@@ -9,3 +10,23 @@ def dataclass_to_dict(obj):
 
 def dataclass_from_dict(cls, data, **kwargs):
     return dacite.from_dict(cls, data, **kwargs)
+
+
+class DataclassYamlDumper(SafeDumper):
+    def ignore_aliases(self, data):
+        return True
+
+    def represent_data(self, data):
+        if dataclasses.is_dataclass(data):
+            return super().represent_dict(dataclass_to_dict(data))
+        return super().represent_data(data)
+
+
+def write_yaml(object, stream):
+    """
+    Writes an object into the output stream as YAML.
+    Serializes dataclasses as dictionaries.
+    """
+    import yaml
+
+    yaml.dump(object, stream, Dumper=DataclassYamlDumper)
