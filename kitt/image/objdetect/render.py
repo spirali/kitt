6620b8f29123458d4202b62cc18fff22ab54549a
Jakub Beránek <berykubik@gmail.com>
2022-04-23 12:36:38 +0200

    Scale text according to canvas size in `render_annotations`

--- kitt/image/objdetect/render.py
+++ kitt/image/objdetect/render.py
@@ -8,8 +8,8 @@ from ..image import get_image_size
 from .annotation import AnnotatedBBox, AnnotationType
 
 DEFAULT_COLOR_MAP = {
-    AnnotationType.GROUND_TRUTH: (255, 255, 255),
-    AnnotationType.PREDICTION: (255, 255, 0),
+    AnnotationType.GROUND_TRUTH: (0, 0, 255),
+    AnnotationType.PREDICTION: (255, 0, 0),
 }
 
 
@@ -26,10 +26,17 @@ def render_annotations(
     if color_fn is None:
         color_fn = color_annotation
 
+    image_size = get_image_size(image)
+    width = image_size[0]
+    bbox_width = min(5, max(1, width // 256))
+    font_scale = width / 1920
+    offset_horizontal = int(20 * (width / 1920))
+    offset_vertical = int(40 * (width / 1920))
+
     for annotation in annotations:
-        box = annotation.bbox.as_denormalized(*get_image_size(image)).to_int()
+        box = annotation.bbox.as_denormalized(*image_size).to_int()
         color = color_fn(annotation)
-        cv2.rectangle(image, box.top_left, box.bottom_right, color, 4)
+        cv2.rectangle(image, box.top_left, box.bottom_right, color, bbox_width)
 
         label = annotation.class_name
         confidence = annotation.confidence
@@ -43,9 +50,12 @@ def render_annotations(
             cv2.putText(
                 image,
                 " ".join(text_fields),
-                (box.top_left[0] + 20, box.top_left[1] + 40),
+                (
+                    box.top_left[0] + offset_horizontal,
+                    box.top_left[1] + offset_vertical,
+                ),
                 cv2.FONT_HERSHEY_SIMPLEX,
-                1,  # font scale
+                font_scale,
                 color,
-                2,  # line type
+                1,  # line type
             )
